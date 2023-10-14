960aa927c3157c6e0ed5006fa417dc10d9d580f9
Kyle Kroger <kyle.s.kroger@gmail.com>
2023-10-13 21:53:31 -0500

    redesign of the gameboard and added logic for it to work

--- src/components/GuessBoard/GuessBoard.tsx
+++ src/components/GuessBoard/GuessBoard.tsx
@@ -4,16 +4,14 @@ import { range } from "@/src/utils";
 
 interface Props {
   guesses: IColor[];
+  secretColorHex: string;
 }
 
-const GuessBoard = ({ guesses }: Props) => {
+const GuessBoard = ({ guesses, secretColorHex }: Props) => {
   return (
     <div className="guess-results">
-      {range(3).map((i) => (
-        <Guess key={i} guess={guesses[i]} type="hsl" />
-      ))}
-      {range(3).map((i) => (
-        <Guess key={i} guess={guesses[i + 3]} type="hex" />
+      {range(5).map((i) => (
+        <Guess key={i} guess={guesses[i]} secretColorHex={secretColorHex} />
       ))}
     </div>
   );
